78199a43a02f0d5ed449f021b265426adb1596e8
dyutidk <39907976+dyutidk@users.noreply.github.com>
2018-11-09 00:47:00 +0530

    Add files via upload

---
+++ homeworks/day2/script.js
@@ -0,0 +1,22 @@
+function read(){
+    var name=document.getElementById("getName").value;
+    var username=document.getElementById("getUsername").value;
+    var addr=document.getElementById("getAddr")
+    
+    var age=document.getElementById("getAge").value;
+    var dob=document.getElementById("getDob").value;
+    var district=document.getElementById("getDist").value;
+    var email=document.getElementById("getEmail").value;
+    var pass=document.getElementById("getPass").value;
+    var cPass=document.getElementById("getCPass").value;
+    alert(name);
+    alert(username);
+    alert(addr);
+    alert(age);
+    alert(district);
+    alert(email); 
+    alert(pass);
+    alert(cpass);
+   
+
+}
